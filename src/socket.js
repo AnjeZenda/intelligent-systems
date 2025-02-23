44b129f1f1d9abef46845fa73efbd65dfd10ab07
AnjeZenda <baykov200396@gmail.com>
2025-02-23 13:39:04 +0300

    add promises

--- src/socket.js
+++ src/socket.js
@@ -1,16 +1,20 @@
 const dgram = require('dgram') // Модуль для аботы с UDP
-module.exports = function(agent, teamName, version) {
+module.exports = async (agent, teamName, version) => {
     // Создание сокета
     const socket = dgram.createSocket({type: 'udp4', reuseAddr: true})
     agent.setSocket(socket) // Задание сокета для агента
     socket.on('message', (msg, info) => {
         agent.msgGot(msg) // Обработка полученного сообщения
     })
-    socket.sendMsg = function(msg) { // Отравка сообщения серверу
-        socket.send(Buffer.from(msg), 6000, 'localhost', (err, bytes) => {
-            if (err) throw err
+    socket.sendMsg = async (msg) => { // Отравка сообщения серверу
+        return new Promise((resolve, reject) => {
+            socket.send(Buffer.from(msg), 6000, 'localhost', (err, bytes) => {
+                console.log(msg)
+                if (err) reject(err)
+                resolve(bytes)
+            })
         })
     }
     // Инициализация игрока на сервере (без параметра goalie)
-    socket.sendMsg(`(init ${teamName} (version ${version}))`)
+    await socket.sendMsg(`(init ${teamName} (version ${version}))`)
 }
