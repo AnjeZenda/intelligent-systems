48c892db304c6647e356b4207c22db3801a6f5e5
YariKar <kyaroslav03@gmail.com>
2025-03-16 12:02:06 +0300

    lb6: first view

--- src/app.js
+++ src/app.js
@@ -1,42 +1,102 @@
 const Agent = require('./agent');
 const Socket = require('./socket');
-const Manager = require("./manager");
-const dt1 = require("./goal_scorer_dt");
-const dt2 = require("./assist_player_dt");
+const low_ctrl = require("./field_player_low");
+const high_ctrl = require("./field_player_high");
 const VERSION = 7;
 
+const goalie_low = require("./ctrl_low");
+const goalie_middle = require("./ctrl_middle");
+const goalie_high = require("./ctrl_high");
+
+function createAgent(team, goalkeeper, controllers, bottom, top, center, start_x, start_y){
+    let agent = new Agent(team, goalkeeper);
+    agent.bottom = bottom;
+    agent.top = top;
+    agent.center = center;
+    agent.controllers = controllers;
+    agent.start_x = start_x;
+    agent.start_y = start_y;
+    return agent;
+}
 
 (async () => {
-	let teamA = "A";
-	let teamB = "B";
-    let player1 = new Agent(teamA);
-    player1.dt = dt2;
-	player1.playerName = "assistant"
-    player1.manager = new Manager();
+    let A_team = [
+        [-40, -20, -35, -40, -30],
+        [-20, 0, -35, -40, -10],
+        [0, 20, -35, -40, 10],
+        [20, 40, 35, -40, 30],
 
-    let player2 = new Agent(teamA);
-    player2.dt = dt1;
-	player2.playerName = "scorer"
-    player2.manager = new Manager();
+        [-40, -20, -25, -25, -30],
+        [-20, 0, -25, -25, -10],
+        [0, 20, -25, -25, 10],
+        [20, 40, -25, -25, 30],
 
-    // let goalKeeper = new Agent(teamB);
-    // goalKeeper.dt = goal_keep_dt;
-    // goalKeeper.manager = new Manager();
-    // goalKeeper.goalie = true;
 
-    await Socket(player1, teamA, VERSION);
-    await Socket(player2, teamA, VERSION);
-    // await Socket(goalKeeper, teamB, VERSION, true);
+        [-40, 0, -10, -10, -20],
+        [0, 40, -10, -10, 20],
+    ]
 
-	let npc1 = new Agent("B");
-    let npc2 = new Agent("B");
-	await Socket(npc1, 'B', VERSION);
-    await Socket(npc2, 'B', VERSION);
+    let B_team = [
+        [-40, -20, -35, -40, 30],
+        [-20, 0, -35, -40, 10],
+        [0, 20, -35, -40, -10],
+        [20, 40, 35, -40, -30],
 
-    await player1.socketSend('move', '-20 0');
-    await player2.socketSend('move', '-20 20');
-    // await goalKeeper.socketSend('move', "-20 0");    
+        [-40, -20, -25, -25, 30],
+        [-20, 0, -25, -25, 10],
+        [0, 20, -25, -25, -10],
+        [20, 40, -25, -25, -30],
 
-    await npc1.socketSend('move', `-57.5 -38`);
-    await npc2.socketSend('move', `-57.5 38`);  
+
+        [-40, 0, -10, -10, 20],
+        [0, 40, -10, -10, -20],
+    ]
+    let players = [];
+    
+    
+    for (const pl of A_team){
+        players.push(createAgent("A", false, [low_ctrl, high_ctrl], 
+            pl[1], pl[0], pl[2], pl[3], pl[4]))
+    }
+    
+    
+    for (const pl of B_team){
+        players.push(createAgent("B", false, [low_ctrl, high_ctrl], 
+            pl[1], pl[0], pl[2], pl[3], pl[4]))
+    }
+    
+
+    
+    let goalkeeper_A = new Agent("A", true);
+    goalkeeper_A.start_x = -50;
+    goalkeeper_A.start_y = 0;
+    let goalkeeper_B = new Agent("B", true);
+    goalkeeper_B.start_x = -50;
+    goalkeeper_B.start_y = 0;
+
+    goalkeeper_A.taken.action = "return";
+    goalkeeper_A.taken.turnData = "ft0";
+    goalkeeper_A.taken.wait = 0;
+
+    goalkeeper_B.taken.action = "return";
+    goalkeeper_B.taken.turnData = "ft0";
+    goalkeeper_B.taken.wait = 0;
+
+
+    goalkeeper_A.controllers = [goalie_low, goalie_middle, goalie_high];
+    goalkeeper_B.controllers = [goalie_low, goalie_middle, goalie_high];
+
+    await Socket(goalkeeper_A, "A", VERSION, true);
+    await goalkeeper_A.socketSend('move', `${goalkeeper_A.start_x} ${goalkeeper_A.start_y}`);
+
+    await Socket(goalkeeper_B, goalkeeper_B.teamName, VERSION, true);
+    await goalkeeper_B.socketSend('move', `${goalkeeper_B.start_x} ${goalkeeper_B.start_y}`);
+
+
+    for (const player of players){
+        await Socket(player, player.teamName, VERSION);
+        //console.log("move ", player.start_x, player.start_y);
+        await player.socketSend('move', `${player.start_x} ${player.start_y}`);
+
+    }
 })();
